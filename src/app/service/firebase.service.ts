b8e65394b3a3b52bd7827e9565d8cf20b94d2206
Marlon Caceres <m_ricky3021@hotmail.com>
2019-01-07 18:09:55 -0500

    #7 User profile

--- src/app/service/firebase.service.ts
+++ src/app/service/firebase.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { AngularFireDatabase } from '@angular/fire/database';
 import { AngularFireAuth } from '@angular/fire/auth';
+import { CookieService } from 'ngx-cookie-service';
 
 @Injectable({
   providedIn: 'root'
@@ -8,11 +9,12 @@ import { AngularFireAuth } from '@angular/fire/auth';
 
 export class FirebaseService {
 
-  constructor(public afDb:AngularFireDatabase, public afAuth:AngularFireAuth) { }
+  constructor(public afDb:AngularFireDatabase, public afAuth:AngularFireAuth , private cookieService:CookieService) { }
 
   createNewUser(mail,pass,data){
     return this.afAuth.auth.createUserWithEmailAndPassword(mail,pass)
     .then(userOk=>{
+      this.cookieService.set( 'userLogged', ''+userOk.user.uid );
       this.afDb.object('user/'+userOk.user.uid).set(data);
     })
     .catch(e=>console.log('Error: ', e ))
@@ -24,5 +26,9 @@ export class FirebaseService {
 
   login(mail,password){
     return this.afAuth.auth.signInWithEmailAndPassword(mail,password)
+  }
+
+  getDataByUser(uid){
+    return this.afDb.object('user/'+uid).valueChanges();
   }
 }
