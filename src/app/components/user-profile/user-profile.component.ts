a54d656f533c98575f4f07dac8f779a612eb68aa
Marlon Caceres <m_ricky3021@hotmail.com>
2019-01-07 18:08:25 -0500

    #7 User profile

---
+++ src/app/components/user-profile/user-profile.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
+import { FirebaseService } from '../../service/firebase.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-user-profile',
+  templateUrl: './user-profile.component.html',
+  styleUrls: ['./user-profile.component.css']
+})
+export class UserProfileComponent implements OnInit {
+
+  userData:any={};
+  constructor(private fireSrv:FirebaseService, private cookieService: CookieService,private _router: Router) { }
+
+  ngOnInit() {
+    if(this.cookieService.check('userLogged')){
+      this.fireSrv.getDataByUser(this.cookieService.get('userLogged')).subscribe(userData=>{
+        this.userData=userData;
+      });
+    }else{
+      this._router.navigate(['/login  ']);
+    }
+  }
+
+}
