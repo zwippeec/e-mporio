72d785d99176aa8e25f89004ba92292d240fceb6
Marlon Caceres <m_ricky3021@hotmail.com>
2019-01-09 09:41:47 -0500

    #9 Edit user profile

--- src/app/components/user-profile/user-profile.component.ts
+++ src/app/components/user-profile/user-profile.component.ts
@@ -11,16 +11,29 @@ import { Router } from '@angular/router';
 export class UserProfileComponent implements OnInit {
 
   userData:any={};
+  isEditing:boolean=false;
+  newUserData:any;
+
   constructor(private fireSrv:FirebaseService, private cookieService: CookieService,private _router: Router) { }
 
   ngOnInit() {
     if(this.cookieService.check('userLogged')){
       this.fireSrv.getDataByUser(this.cookieService.get('userLogged')).subscribe(userData=>{
         this.userData=userData;
+        this.newUserData=userData;
       });
     }else{
       this._router.navigate(['/login  ']);
     }
   }
 
+  editProfile(){
+    this.isEditing=true;
+  }
+
+  saveNewData(){
+    this.isEditing=false;
+    
+    this.fireSrv.updateProfile(this.newUserData,this.cookieService.get('userLogged'));
+  }
 }
