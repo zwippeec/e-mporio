cc91acfd807e01b8515ecf1575a6b9c3ee85b8cf
Marlon Caceres <m_ricky3021@hotmail.com>
2019-02-19 10:15:34 -0500

    #79 Quantity

--- src/app/components/shopping-cart/shopping-cart.component.ts
+++ src/app/components/shopping-cart/shopping-cart.component.ts
@@ -187,26 +187,31 @@ export class ShoppingCartComponent implements OnInit {
     })
   }
 
-  addCart(Pid,typeData){
+  addCart(Pid,typeData,quantity){
+    //this.showInfoCart=false;
     let items:any=[];//array to first object
     let _tmpList:any=[];//temporal array to list cart
+
+    if(quantity==undefined){
+      quantity=1;
+    }
+
     //Condition if exits listCart or create new list
     if(localStorage.getItem('listCart')!=null){
       _tmpList=JSON.parse(localStorage.getItem('listCart'));//get list cart
       //Condition to add item or increase quantity on item
       if(_tmpList.findIndex(data=>data.id===Pid && data.type===typeData)!=-1){
-        _tmpList[_tmpList.findIndex(data=>data.id===Pid && data.type===typeData)].quantity+=1;//increase quantity on item 
+        _tmpList[_tmpList.findIndex(data=>data.id===Pid && data.type===typeData)].quantity+=quantity;//increase quantity on item 
       }else{
-        _tmpList.push({id:Pid,quantity:1,type:typeData})//add item on list if no exist
+        _tmpList.push({id:Pid,quantity:quantity,type:typeData})//add item on list if no exist
       } 
       localStorage.removeItem('listCart');//remove old list
       localStorage.setItem('listCart',JSON.stringify(_tmpList))//create new list
-      this.ngOnInit();
     }else{
-      items.push({id:Pid,quantity:1,type:typeData});//first item on list
+      items.push({id:Pid,quantity:quantity,type:typeData});//first item on list
       localStorage.setItem('listCart',JSON.stringify(items))//create list
-      this.ngOnInit();
     }
+    this.ngOnInit();
   }
 
   scroll(){
